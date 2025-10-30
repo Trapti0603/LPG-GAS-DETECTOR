4eae293c0a7d85bafb1747a339a492c48bf8ff41
Trapti Upadhyay <shelendraupadhyay7@gmail.com>
2025-10-31 00:51:17 +0530

    Add files via upload
    This file consist of main code through which run the arduino and detect the LPG Gas and we can also detect other gases and substances like Ammonia(NH3),Sulfur(S),Benzene(C6H6), CO2 and other harmful gases and smoke.

---
+++ sketch_sep25a.ino
@@ -0,0 +1,71 @@
+#include<Servo.h>
+
+Servo servo;
+
+int smokeA0=A0;
+
+int buzzer =11;
+
+int led = 12;
+
+float sensorValue;
+
+void setup() {
+  pinMode(buzzer,OUTPUT);
+
+  pinMode(led, OUTPUT);
+
+  pinMode(smokeA0,INPUT);
+
+   Serial.begin(9600);  
+
+  Serial.println("Gas sensor warming up!");
+
+  delay(20000);  
+
+  servo.attach(8);
+
+  servo.write(0);
+
+  delay(2000);
+
+}
+
+void loop() {
+    sensorValue=analogRead(smokeA0);
+
+  if(sensorValue > 600)
+
+  {
+
+    Serial.print(  "Smoke detected!");
+
+    digitalWrite(buzzer,HIGH);
+
+    digitalWrite(led,HIGH);
+
+    servo.write(90);
+
+    delay(1000);
+
+  }
+
+  else
+
+  {
+
+     Serial.print(  "Smoke  not detected!");
+
+    digitalWrite(buzzer, LOW);
+
+    digitalWrite(led,LOW);
+
+    servo.write(0);
+    delay(1000);
+
+  }
+
+  delay(2000);  
+
+
+}
